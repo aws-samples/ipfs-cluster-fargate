bf24440f23f2ea5ac2172c2c3565e35ecd89d751
Nico <menciere@amazon.com>
2024-01-20 15:08:06 -0500

    Cleanup Dockerfile_efs

--- docker/001-config_efs.sh
+++ docker/001-config_efs.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -ex
 
-# This script is an attempt to cleanup the temporary files when the daemon restarts
-# Sometimes the deamon would not shutdown correctly leaving the temp files and preventing a restart
-# However, those temp files would sometimes be owned by root, rendering this script useless as it's run by the 'ipfs' user
-
-# We also rely on the entrypoint.sh which runs as root to cleanup those files
-
+# We clean the lock files and temp files that could prevent correct startup
 rm -rfv /data/ipfs/repo.lock /data/ipfs/datastore/LOCK /data/ipfs/blocks/.temp /data/ipfs/datastore/LOG
